0d3992932e2c5f1afbb75f6e073f59fabbeae0c5
Dave Falkner <ardave@gmail.com>
2018-03-01 20:56:20 -0800

    splitting out more code

--- src/Plot.Core/LineChart.Rendering.fs
+++ src/Plot.Core/LineChart.Rendering.fs
@@ -1,7 +1,8 @@
 namespace Plot.Core.LineChart
-
     module internal Rendering =
-
+        open SixLabors.Fonts
+        open SixLabors.ImageSharp
+        open SixLabors.ImageSharp.Drawing
         open SixLabors.Primitives
         open SixLabors.Shapes
         open Plot.Core
@@ -23,3 +24,37 @@ namespace Plot.Core.LineChart
         let internal addLinesF (points:FittedPoint array) (pb:PathBuilder) =
             let pointFs = points |> Array.map fittedToOriginal
             addLines pointFs pb
+
+        let internal drawMaxY minMaxes upperLeft (font:Font) (ctx:IImageProcessingContext<Rgba32>) =
+            let maxYStr = minMaxes.maxY.ToString()
+            let pbText2 = PathBuilder()
+            let p1 = PointF(float32 upperLeft.x - font.Size * float32 maxYStr.Length, float32 upperLeft.y)
+            let p2 = PointF(float32 upperLeft.x, float32 upperLeft.y)
+            pbText2.AddLine(p1, p2) |> ignore
+            let path = pbText2.Build()
+            ctx.DrawText(maxYStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length)) |> ignore
+
+        let internal drawMinY minMaxes upperLeft lowerRight (font:Font) (ctx:IImageProcessingContext<Rgba32>) =
+            let minYStr = minMaxes.minY.ToString()
+            let pbText = PathBuilder()
+            pbText.AddLine(PointF(float32 upperLeft.x - font.Size * float32 minYStr.Length, float32 lowerRight.y), lowerRight |> originalToPointF) |> ignore
+            let path = pbText.Build()
+            ctx.DrawText(minYStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length))  |> ignore
+
+        let internal drawMinX minMaxes upperLeft lowerRight (font:Font) (ctx:IImageProcessingContext<Rgba32>) = 
+            let minXStr = minMaxes.minX.originalValue.ToString()
+            let pb = PathBuilder()
+            let p3 = PointF(float32 upperLeft.x , float32 lowerRight.y + font.Size)
+            let p4 = PointF(float32 lowerRight.x, float32 lowerRight.y + font.Size)
+            pb.AddLine(p3, p4) |> ignore
+            let path = pb.Build()
+            ctx.DrawText(minXStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length)) |> ignore
+
+        let internal drawMaxX minMaxes lowerRight (font:Font) (ctx:IImageProcessingContext<Rgba32>) =
+            let maxXStr = minMaxes.maxX.originalValue.ToString()
+            let pbText4 = PathBuilder()
+            let p5 = PointF(float32 lowerRight.x - font.Size * float32 maxXStr.Length * 0.5f, float32 lowerRight.y + font.Size)
+            let p6 = PointF(float32 lowerRight.x + font.Size * float32 maxXStr.Length * 0.5f, float32 lowerRight.y + font.Size)
+            pbText4.AddLine(p5, p6) |> ignore
+            let path = pbText4.Build()
+            ctx.DrawText(maxXStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length)) |> ignore
--- src/Plot.Core/LineChart.fs
+++ src/Plot.Core/LineChart.fs
@@ -29,60 +29,6 @@ module LineChart =
         pb |> addLines [|p1; p2; p3|]
         p1, p3
 
-
-    let internal fitPointsToGrid upperLeft lowerRight firstPoint points =
-        let minMaxes = getMinMaxes firstPoint points
-        let pointWidth  = minMaxes.maxX.value - minMaxes.minX.value
-        let pointHeight = minMaxes.maxY - minMaxes.minY
-        let chartWidth  = lowerRight.x - upperLeft.x
-        let chartHeight = lowerRight.y - upperLeft.y
-
-        let fittedPoints =
-            points
-            |> Array.map(fun point ->
-                let pctW = (point.x - minMaxes.minX.value) / pointWidth
-                let pctH = (minMaxes.maxY - point.y) / pointHeight
-                { 
-                    fittedX = pctW * chartWidth + upperLeft.x
-                    fittedY = pctH * chartHeight + upperLeft.y
-                }
-            )
-        fittedPoints, minMaxes
-
-    let private drawMaxY minMaxes upperLeft (font:Font) (ctx:IImageProcessingContext<Rgba32>) =
-        let maxYStr = minMaxes.maxY.ToString()
-        let pbText2 = PathBuilder()
-        let p1 = PointF(float32 upperLeft.x - font.Size * float32 maxYStr.Length, float32 upperLeft.y)
-        let p2 = PointF(float32 upperLeft.x, float32 upperLeft.y)
-        pbText2.AddLine(p1, p2) |> ignore
-        let path = pbText2.Build()
-        ctx.DrawText(maxYStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length)) |> ignore
-
-    let private drawMinY minMaxes upperLeft lowerRight (font:Font) (ctx:IImageProcessingContext<Rgba32>) =
-        let minYStr = minMaxes.minY.ToString()
-        let pbText = PathBuilder()
-        pbText.AddLine(PointF(float32 upperLeft.x - font.Size * float32 minYStr.Length, float32 lowerRight.y), lowerRight |> originalToPointF) |> ignore
-        let path = pbText.Build()
-        ctx.DrawText(minYStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length))  |> ignore
-
-    let private drawMinX minMaxes upperLeft lowerRight (font:Font) (ctx:IImageProcessingContext<Rgba32>) = 
-        let minXStr = minMaxes.minX.originalValue.ToString()
-        let pb = PathBuilder()
-        let p3 = PointF(float32 upperLeft.x , float32 lowerRight.y + font.Size)
-        let p4 = PointF(float32 lowerRight.x, float32 lowerRight.y + font.Size)
-        pb.AddLine(p3, p4) |> ignore
-        let path = pb.Build()
-        ctx.DrawText(minXStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length)) |> ignore
-
-    let private drawMaxX minMaxes lowerRight (font:Font) (ctx:IImageProcessingContext<Rgba32>) =
-        let maxXStr = minMaxes.maxX.originalValue.ToString()
-        let pbText4 = PathBuilder()
-        let p5 = PointF(float32 lowerRight.x - font.Size * float32 maxXStr.Length * 0.5f, float32 lowerRight.y + font.Size)
-        let p6 = PointF(float32 lowerRight.x + font.Size * float32 maxXStr.Length * 0.5f, float32 lowerRight.y + font.Size)
-        pbText4.AddLine(p5, p6) |> ignore
-        let path = pbText4.Build()
-        ctx.DrawText(maxXStr, font, Rgba32.Black, path, TextGraphicsOptions(true, WrapTextWidth = path.Length)) |> ignore
-
     let private fillBackground (ctx:IImageProcessingContext<Rgba32>) =
         ctx.Fill(Rgba32.White) |> ignore
 
