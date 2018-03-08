ecad791f1c69c5ddfaddbbea6182c902d3740f38
Dave Falkner <ardave@gmail.com>
2018-03-08 14:44:41 -0800

    Switched from xunit to mstest due to package versioning challenges

--- src/Tests/ArrayExtensionsTests.fs
+++ src/Tests/ArrayExtensionsTests.fs
@@ -1,24 +1,24 @@
-module ArrayExtensionsTests
-open Xunit
-open FsUnit.Xunit
+namespace Tests
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
 open Plot.Core
+open Helpers
 
-[<Fact>]
-let ``tryGet should return None if array is empty`` () =
-    let opt = [||] |> Array.tryGet 0
-    opt.IsNone |> should be True
+[<TestClass>]
+type ArrayExtensionsTests () =
+    [<TestMethod>]
+    member this.``tryGet should return None if array is empty`` () =
+        let opt = [||] |> Array.tryGet 0
+        opt.IsNone |> shouldEqual true
 
-[<Fact>]
-let ``tryGet should return Some element if the element's array index is requested``() =
-    [| "foo" |] 
-    |> Array.tryGet 0
-    |> Option.isSome
-    |> should be True
+    [<TestMethod>]
+    member this.``tryGet should return Some element if the element's array index is requested``() =
+        [| "foo" |] 
+        |> Array.tryGet 0
+        |> Option.isSome
+        |> shouldEqual true
 
-    [| "foo" |]
-    |> Array.tryGet 1
-    |> Option.isNone
-    |> should be True
-
-[<EntryPoint>]
-let main _ = 0
+        [| "foo" |]
+        |> Array.tryGet 1
+        |> Option.isNone
+        |> shouldEqual true
--- src/Tests/LineChartTests.fs
+++ src/Tests/LineChartTests.fs
@@ -1,57 +1,61 @@
-module LineChartTests
+namespace Tests
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
 open SixLabors.ImageSharp
-open Xunit
-open FsUnit.Xunit
 open Plot.Core
 open Plot.Core.LineChart
 open Plot.Core.Settings
+open Helpers
 
-[<Fact>]
-let ``Generate actual image from air passenger data``() =
-    let points   = Plot.Core.FakeData.hourlyDataDateTimes
-    let settings = createLineChartSettings "Air Passenger Data" 1500 500
-    let imageOpt = points |> createLineChart settings
+[<TestClass>]
+type LineChartTests () =
+    [<TestMethod>]
+    member this.``Generate actual image from air passenger data`` () =
+        let points   = Plot.Core.FakeData.hourlyDataDateTimes
+        let settings = createLineChartSettings "Air Passenger Data" 1500 500
+        let imageOpt = points |> createLineChart settings
 
-    match imageOpt with
-    | Some img -> img.Save "AirPassengerData.png"
-    | None -> failwith "Maybe you didn't include any points for your chart"
+        match imageOpt with
+        | Some img -> img.Save "AirPassengerData.png"
+        | None -> failwith "Maybe you didn't include any points for your chart"
 
-[<Fact>]
-let ``getMinMaxes should get the correct min maxes``() =
-    let chartPoints =
-        [|
-            { originalX = 1.f; x = 1.f; y = 2.f  }
-            { originalX = 4.f; x = 4.f; y = 5.f  }
-            { originalX = 7.f; x = 7.f; y = 8.f  }
-        |]
+    [<TestMethod>]
+    member this.``getMinMaxes should get the correct min maxes``() =
+        let chartPoints =
+            [|
+                { originalX = 1.f; x = 1.f; y = 2.f  }
+                { originalX = 4.f; x = 4.f; y = 5.f  }
+                { originalX = 7.f; x = 7.f; y = 8.f  }
+            |]
 
-    let minMaxes = chartPoints |> getMinMaxes chartPoints.[0]
+        let minMaxes = chartPoints |> getMinMaxes chartPoints.[0]
 
-    minMaxes.minX.originalValue |> should equal 1.f
-    minMaxes.maxX.originalValue |> should equal 7.f
-    minMaxes.minY |> should equal 2.f
-    minMaxes.maxY |> should equal 8.f
+        minMaxes.minX.originalValue |> shouldEqual 1.f
+        minMaxes.maxX.originalValue |> shouldEqual 7.f
+        minMaxes.minY |> shouldEqual 2.f
+        minMaxes.maxY |> shouldEqual 8.f
 
-[<Fact>]
-let ``fitPointsToGrid should fit the points correctly``() =
-    let upperLeft = pointf 150.f 50.f
-    let lowerRight = pointf 1350.f 450.f
-    let points = FakeData.hourlyDataDateTimes
-    let fittedPoints, _ = fitPointsToGrid upperLeft lowerRight points.[0] points
-    
-    let fartherLeftThanUpperLeft   p = p.fittedX < upperLeft.X
-    let fartherUpThanUpperLeft     p = p.fittedY < upperLeft.Y
-    let fartherRightThanLowerRight p = p.fittedX > lowerRight.X
-    let fartherDownThanLowerRight  p = p.fittedY > lowerRight.Y
+    [<TestMethod>]
+    member this.``fitPointsToGrid should fit the points correctly``() =
+        let upperLeft = pointf 150.f 50.f
+        let lowerRight = pointf 1350.f 450.f
+        let points = FakeData.hourlyDataDateTimes
+        let fittedPoints, _ = fitPointsToGrid upperLeft lowerRight points.[0] points
+        
+        let fartherLeftThanUpperLeft   p = p.fittedX < upperLeft.X
+        let fartherUpThanUpperLeft     p = p.fittedY < upperLeft.Y
+        let fartherRightThanLowerRight p = p.fittedX > lowerRight.X
+        let fartherDownThanLowerRight  p = p.fittedY > lowerRight.Y
 
-    [
-        fartherLeftThanUpperLeft
-        fartherUpThanUpperLeft
-        fartherRightThanLowerRight
-        fartherDownThanLowerRight
-    ]
-    |> List.iter (fun f ->
-        fittedPoints
-        |> Array.exists f
-        |> should be False
-    )
+        [
+            fartherLeftThanUpperLeft
+            fartherUpThanUpperLeft
+            fartherRightThanLowerRight
+            fartherDownThanLowerRight
+        ]
+        |> List.iter (fun f ->
+            fittedPoints
+            |> Array.exists f
+            |> shouldEqual false
+        )
