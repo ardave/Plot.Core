4a4b72dd9444a174a5983f3db94a5b3abb5e0e1c
Falkner, David <David.B.Falkner@nordstrom.com>
2018-03-15 12:58:35 -0700

    refactoring

--- src/Plot.Core/LineChart.fs
+++ src/Plot.Core/LineChart.fs
@@ -31,19 +31,20 @@ module LineChart =
         let img = new Image<Rgba32>(settings.Width, settings.Height)
         let upperLeft, lowerRight, drawMajorGridLinesFunc = drawMajorGridLines settings img
 
-        let backgroundMutations = [
-            fillBackground
-            drawMajorGridLinesFunc
-            drawTitle settings
-            // drawLegend seriesList settings
-        ]
-
-        seriesList
-        |> List.iter(fun series ->
-            match series.originalPoints |> Array.tryGet 0 with
-            | None -> ()
-            | Some firstPoint ->
+        match seriesList |> List.tryHead with
+        | None             -> ()
+        | Some firstSeries ->
+            match firstSeries.originalPoints |> Array.tryGet 0 with
+            | None      -> ()
+            | Some firstPoint -> 
                 let scaledSeriesList, minMaxes, scalingFactors = scalePointsToGrid upperLeft lowerRight firstPoint seriesList
+
+                let backgroundMutations = [
+                    fillBackground
+                    drawMajorGridLinesFunc
+                    drawTitle settings
+                    // drawLegend seriesList settings
+                ]
                 let drawSeriesFuncs = scaledSeriesList |> List.map drawDataLines
                 let drawMinorGridLinesFunc = assembleMinorGridLinesFunctions settings scalingFactors
                 let allMutations = backgroundMutations @ drawSeriesFuncs @[
@@ -53,10 +54,8 @@ module LineChart =
                                                 drawMaxY minMaxes upperLeft settings.Font
                                                 drawMinorGridLinesFunc
                                                 
-                ] 
-                
+                ]
                 img.Mutate(fun ctx ->
-                    allMutations |> List.iter(fun m -> m ctx)
-            )
-        )
+                    allMutations |> List.iter(fun m -> m ctx))
         img
+
